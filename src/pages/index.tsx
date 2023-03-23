a28de7775edf22d48379db3f04cce37c9618f9f2
Satyam Kulkarni <punekar.satyam@gmail.com>
2023-03-23 15:45:06 +0530

    added new wiam

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect } from "react";
-import WIAM from "iam-react";
+import WIAM from "wiam";
 import { HomeView } from "../views";
 
 const wiam = new WIAM("930aa070-9535-484f-b33b-8e4557765022", {
--- src/wiam/config.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = "https://web3-iam-production-0d26.up.railway.app"
